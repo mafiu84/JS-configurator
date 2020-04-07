edfe9193990b69471a7393b17d88204f02465d73
Mateusz Poplewski <matheush84@gmail.com>
2020-04-07 17:40:00 +0000

    first question works properly

--- js/main.js
+++ js/main.js
@@ -1,39 +1,18 @@
 function setRange(Boat) {
 
-    var dropbox = document.BoatConfigurator.Range_type;
+    var dropbox = document.BoatConfigurator.Range;
 
     dropbox.options.length = 0;
 
-    if (Boat == "1") {
+    if (Boat == "1" || Boat == "2") {
 
         dropbox.options[dropbox.options.length] = new Option('Shell', '1');
 
-        dropbox.options[dropbox.options.length] = new Option
+        dropbox.options[dropbox.options.length] = new Option('Sailaway', '2');
 
-            ('Sailaway', '2');
-        dropbox.options[dropbox.options.length] = new Option
+        dropbox.options[dropbox.options.length] = new Option('Sailaway lined', '3');
 
-            ('Sailaway lined', '3');
-        dropbox.options[dropbox.options.length] = new Option
-
-            ('Fully fitted', '4');
+        dropbox.options[dropbox.options.length] = new Option('Fully fitted', '4');
 
     }
-
-    if (Boat == "2") {
-
-        dropbox.options[dropbox.options.length] = new Option('Pioneer', '5');
-
-        dropbox.options[dropbox.options.length] = new Option('Monarch/Emperor', '6');
-
-        dropbox.options[dropbox.options.length] = new Option('Baby euro', '7');
-
-        dropbox.options[dropbox.options.length] = new Option('Euro', '8');
-
-        dropbox.options[dropbox.options.length] = new Option('Hoxton', '9');
-
-        dropbox.options[dropbox.options.length] = new Option('Abode', '10');
-
-    }
-
 }
